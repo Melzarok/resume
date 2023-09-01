ee0b64b2d7ef0101b1114ec74b33c895584a24b8
Melzarok <timurmilutin727@gmail.com>
2023-09-01 17:20:55 +0300

    Bootstrap Testing container

--- src/route/index.js
+++ src/route/index.js
@@ -1086,5 +1086,17 @@ router.get('/facebook', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+
+////////////////////////////////////////////////////
+
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrapTest', {
+    layout: 'bootstrapTest',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
